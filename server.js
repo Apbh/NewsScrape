56f2fb1ca5b392a941c50e183a7205f40fd9da2f
Apbh <bhattacharya_a@hotmail.com>
2018-10-04 22:37:28 -0400

    added node modules

--- server.js
+++ server.js
@@ -24,8 +24,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+
 // Connect to the Mongo DB
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
@@ -42,10 +44,6 @@ app.get("/", function(req, res) {
 app.get("/savedArticles", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/savedArticles.html"));
 });
-
-// app.get("/allArticles", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./public/showArticles.html"));
-// });
 
 //cheerio
 app.get("/scrape", function(req, res) {
